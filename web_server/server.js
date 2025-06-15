8571bc6bf983e9f3632ac6d9a20c8ac70bd1d814
ambaparmar07 <amba.parmar07@gmail.com>
2025-06-15 13:59:02 +0530

    fourth assignment

---
+++ web_server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is the About Page!');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
